7718d69c56cf4f650ac3b05115f0ffc5b62917ac
Behnam <behnam.fathi78@gmail.com>
2018-08-05 16:36:07 +0430

    Merge

--- app/src/main/java/ir/woope/woopeapp/helpers/Constants.java
+++ app/src/main/java/ir/woope/woopeapp/helpers/Constants.java
@@ -15,11 +15,7 @@ public final class Constants {
         public HTTP() {
 
         }
-        public static final String BASE_URL = "http://localhost:58795/";
 
-//        public static final String BASE_URL = "http://aronapp.ir:8080/";
-//        public static final String BASE_URL = "http://aronapp.ir:8880/";
-// test
         public static final String BASE_URL = "http://192.168.100.5:8090/";
 //        public static final String BASE_URL = "http://localhost/";
     }
