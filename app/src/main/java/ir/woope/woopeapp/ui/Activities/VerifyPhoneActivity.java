7a37da855a47391382163c4c776e6e3cee6a247c
Behnam <bfp135531@gmail.com>
2018-12-11 16:48:09 +0330

    Fixed PhoneVerification After Login

--- app/src/main/java/ir/woope/woopeapp/ui/Activities/VerifyPhoneActivity.java
+++ app/src/main/java/ir/woope/woopeapp/ui/Activities/VerifyPhoneActivity.java
@@ -12,6 +12,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.balysv.materialripple.MaterialRippleLayout;
+import com.chaos.view.PinView;
+import com.wang.avi.AVLoadingIndicatorView;
+
 import ir.woope.woopeapp.R;
 import ir.woope.woopeapp.helpers.Constants;
 import ir.woope.woopeapp.helpers.Utility;
@@ -37,6 +41,16 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
     String phoneNumber;
 
+    ProgressBar progress_bar;
+
+    TextView countdown_timer;
+    TextView resend;
+    MaterialRippleLayout accept;
+
+    AVLoadingIndicatorView progress;
+
+    PinView code;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +60,13 @@ public class VerifyPhoneActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final String token = intent.getStringExtra(TOKEN);
 
-        final TextView countdown_timer = (TextView) findViewById(R.id.txt_countdown_changepass);
-//        final EditText code = (EditText) findViewById(R.id.txtbx_confirm_code_register);
-//        final ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar_sms_verif_register);
+        countdown_timer = (TextView) findViewById(R.id.txt_countdown_register);
+
+        progress = findViewById(R.id.progressBar_sms_verification_register);
+
+        progress_bar = findViewById(R.id.sms_validation_register_progressBar);
+
+        code = findViewById(R.id.pinView_smsValidation_register);
 
         retrofit_getProfile = new Retrofit.Builder()
                 .baseUrl(Constants.HTTP.BASE_URL)
@@ -64,216 +82,221 @@ public class VerifyPhoneActivity extends AppCompatActivity {
 
         final RegisterInterface send = retrofit_sendConfirmCode.create(RegisterInterface.class);
 
-        final Button accept = (Button) findViewById(R.id.btn_accept_code_register);
-//        final Button resend = (Button) findViewById(R.id.btn_resendcode_register);
+        accept = findViewById(R.id.btn_accept_code_register);
+        resend = findViewById(R.id.btn_resendcode_register);
 
-//        final CountDownTimer ch = new CountDownTimer(60000, 1000) {
-//            @Override
-//            public void onTick(long millisUntilFinished) {
-//
-//                countdown_timer.setText(getText(R.string.remaining_time) + " : " + millisUntilFinished / 1000);
-//
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//                countdown_timer.setText("");
-//                if (code.equals("")) {
-//
-//                    countdown_timer.setText("زمان شما به پایان رسید");
-//
-////                    Toast.makeText(
-////                            sms_validation.this
-////                            , "زمان شما به پایان رسید!",
-////                            Toast.LENGTH_SHORT).show();
-//
-//                    finish();
-//                }
-//                accept.setVisibility(View.GONE);
-//                resend.setVisibility(View.VISIBLE);
-//            }
-//        }.start();
-//
-//        getInfo.getProfileFromServer("bearer " + token).enqueue(new Callback<Profile>() {
-//            @Override
-//            public void onResponse(Call<Profile> call, Response<Profile> response) {
-//
-//                if (response.code() == 200) {
-//
-//                    phoneNumber = response.body().getMobile();
-//
-//                    send.send_code(phoneNumber).enqueue(new Callback<ApiResponse>() {
-//                        @Override
-//                        public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-//
-//                            if (response.body().getStatus() == 101) {
-//
-//
-//                                Toast.makeText(
-//                                        VerifyPhoneActivity.this
-//                                        , response.body().getMessage(),
-//                                        Toast.LENGTH_SHORT).show();
-//
-//
-//                            }
-//
-//                        }
-//
-//                        @Override
-//                        public void onFailure(Call<ApiResponse> call, Throwable t) {
-//
-//                            Toast.makeText(
-//                                    VerifyPhoneActivity.this
-//                                    , "خطای اتصال",
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//
-//                } else {
-//                    Toast.makeText(
-//
-//                            VerifyPhoneActivity.this
-//                            , "خطا",
-//                            Toast.LENGTH_SHORT).show();
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Profile> call, Throwable t) {
-//
-//                progress.setVisibility(View.GONE);
-//
-//                Toast.makeText(
-//
-//                        VerifyPhoneActivity.this
-//                        , "خطای اتصال",
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//
-//
-//
-//        resend.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View arg0) {
-//
-//                resend.setVisibility(View.GONE);
-//                progress.setVisibility(View.VISIBLE);
-//
-//                send.send_code(phoneNumber).enqueue(new Callback<ApiResponse>() {
-//                    @Override
-//                    public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-//
-//                        if (response.body().getStatus() == 101) {
-//
-//                            ch.start();
-//
-//                            progress.setVisibility(View.GONE);
-//                            resend.setVisibility(View.GONE);
-//                            accept.setVisibility(View.VISIBLE);
-//                            Toast.makeText(
-//                                    VerifyPhoneActivity.this
-//                                    , response.body().getMessage(),
-//                                    Toast.LENGTH_SHORT).show();
-//
-//
-//                        } else {
-//
-//                            resend.setVisibility(View.VISIBLE);
-//                            progress.setVisibility(View.GONE);
-//                            Toast.makeText(
-//                                    VerifyPhoneActivity.this
-//                                    , response.body().getMessage(),
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-//
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<ApiResponse> call, Throwable t) {
-//
-//                        resend.setVisibility(View.VISIBLE);
-//                        progress.setVisibility(View.GONE);
-//
-//                        Toast.makeText(
-//                                VerifyPhoneActivity.this
-//                                , "خطای اتصال",
-//                                Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//            }
-//        });
-//
-//
-//        accept.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View arg0) {
-//
-//                progress.setVisibility(View.VISIBLE);
-//                accept.setVisibility(View.GONE);
-//
-//                send.send_verif_code(phoneNumber, Utility.arabicToDecimal(code.getText().toString())).enqueue(new Callback<ApiResponse>() {
-//                    @Override
-//                    public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-//
-//                        if (response.body().getStatus() == 101) {
-//
-//                            SharedPreferences settings = getApplicationContext().getSharedPreferences(Constants.GlobalConstants.MY_SHARED_PREFERENCES, MODE_PRIVATE);
-//                            SharedPreferences.Editor editor = settings.edit();
-//                            editor.putString(TOKEN, token);
-//                            editor.apply();
-//
-//                            progress.setVisibility(View.GONE);
-//
-//                            Toast.makeText(
-//                                    VerifyPhoneActivity.this
-//                                    , response.body().getMessage(),
-//                                    Toast.LENGTH_SHORT).show();
-//
-//                            Intent goto_mainpage = new Intent(VerifyPhoneActivity.this,
-//                                    MainActivity.class);
-//                            goto_mainpage.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                            finish();
-//                            startActivity(goto_mainpage);
-//
-//
-//                        } else {
-//
-//                            accept.setVisibility(View.VISIBLE);
-//                            progress.setVisibility(View.GONE);
-//
-//                            Toast.makeText(
-//                                    VerifyPhoneActivity.this
-//                                    , response.body().getMessage(),
-//                                    Toast.LENGTH_SHORT).show();
-//
-//                        }
-//
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<ApiResponse> call, Throwable t) {
-//                        // این متود هم فقط زمانی فرخوانی می‌شه که به هر دلیلی کانکشن ما با مشکل روبرو بشه
-//
-//                        accept.setVisibility(View.VISIBLE);
-//                        progress.setVisibility(View.GONE);
-//
-//                        Toast.makeText(
-//                                VerifyPhoneActivity.this
-//                                , "خطا اتصال!",
-//                                Toast.LENGTH_SHORT).show();
-//
-//
-//                    }
-//
-//                });
-//
-//            }
-//
-//        });
+        timer();
+
+        getInfo.getProfileFromServer("bearer " + token).enqueue(new Callback<Profile>() {
+            @Override
+            public void onResponse(Call<Profile> call, Response<Profile> response) {
+
+                if (response.code() == 200) {
+
+                    phoneNumber = response.body().getMobile();
+
+                    send.send_code(phoneNumber).enqueue(new Callback<ApiResponse>() {
+                        @Override
+                        public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+
+                            if (response.body().getStatus() == 101) {
+
+
+                                Toast.makeText(
+                                        VerifyPhoneActivity.this
+                                        , response.body().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<ApiResponse> call, Throwable t) {
+
+                            Toast.makeText(
+                                    VerifyPhoneActivity.this
+                                    , "خطای اتصال",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(
+
+                            VerifyPhoneActivity.this
+                            , "خطا",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Profile> call, Throwable t) {
+
+                progress.setVisibility(View.GONE);
+
+                Toast.makeText(
+
+                        VerifyPhoneActivity.this
+                        , "خطای اتصال",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+        resend.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                resend.setVisibility(View.GONE);
+                progress.setVisibility(View.VISIBLE);
+
+                send.send_code(phoneNumber).enqueue(new Callback<ApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+
+                        if (response.body().getStatus() == 101) {
+
+                            timer();
+
+                            progress.setVisibility(View.GONE);
+                            resend.setVisibility(View.GONE);
+                            accept.setVisibility(View.VISIBLE);
+                            Toast.makeText(
+                                    VerifyPhoneActivity.this
+                                    , response.body().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+
+                            resend.setVisibility(View.VISIBLE);
+                            progress.setVisibility(View.GONE);
+                            Toast.makeText(
+                                    VerifyPhoneActivity.this
+                                    , response.body().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ApiResponse> call, Throwable t) {
+
+                        resend.setVisibility(View.VISIBLE);
+                        progress.setVisibility(View.GONE);
+
+                        Toast.makeText(
+                                VerifyPhoneActivity.this
+                                , "خطای اتصال",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+
+        accept.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                progress.setVisibility(View.VISIBLE);
+                accept.setVisibility(View.GONE);
+
+                send.send_verif_code(phoneNumber, Utility.arabicToDecimal(code.getText().toString())).enqueue(new Callback<ApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+
+                        if (response.body().getStatus() == 101) {
+
+                            SharedPreferences settings = getApplicationContext().getSharedPreferences(Constants.GlobalConstants.MY_SHARED_PREFERENCES, MODE_PRIVATE);
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putString(TOKEN, token);
+                            editor.apply();
+
+                            progress.setVisibility(View.GONE);
+
+                            Toast.makeText(
+                                    VerifyPhoneActivity.this
+                                    , response.body().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                            Intent goto_mainpage = new Intent(VerifyPhoneActivity.this,
+                                    MainActivity.class);
+                            goto_mainpage.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            finish();
+                            startActivity(goto_mainpage);
+
+
+                        } else {
+
+                            accept.setVisibility(View.VISIBLE);
+                            progress.setVisibility(View.GONE);
+
+                            Toast.makeText(
+                                    VerifyPhoneActivity.this
+                                    , response.body().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ApiResponse> call, Throwable t) {
+                        // این متود هم فقط زمانی فرخوانی می‌شه که به هر دلیلی کانکشن ما با مشکل روبرو بشه
+
+                        accept.setVisibility(View.VISIBLE);
+                        progress.setVisibility(View.GONE);
+
+                        Toast.makeText(
+                                VerifyPhoneActivity.this
+                                , "خطا اتصال!",
+                                Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                });
+
+            }
+
+        });
+
+    }
+
+    public void timer(){
+
+        new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //this will be done every 1000 milliseconds ( 1 seconds )
+
+                countdown_timer.setText("00:"+millisUntilFinished / 1000);
+
+                long progress = (60000 - millisUntilFinished) / 1000;
+                progress_bar.setProgress((int) progress);
+            }
+
+            @Override
+            public void onFinish() {
+                //the progressBar will be invisible after 60 000 miliseconds ( 1 minute)
+
+                countdown_timer.setText("00:00");
+
+                progress_bar.setProgress(60);
+
+                resend.animate().alpha(1.0f);
+                resend.setVisibility(View.VISIBLE);
+
+            }
+
+        }.start();
 
     }
 
